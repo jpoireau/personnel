2ead9727f2cf55e50a24bd69fe6c5a6020681c81
RAMSES-666 <72388998+RAMSES-666@users.noreply.github.com>
2020-11-23 11:37:17 +0100

    Add files via upload

---
+++ INSERT-personnel.sql
@@ -0,0 +1,15 @@
+INSERT INTO LIGUE values(01,"ligue de Vo Thùat");
+INSERT INTO LIGUE values(02,"ligue de Taekwondo");
+INSERT INTO LIGUE values(03,"ligue de mangeur de hot dog");
+
+INSERT INTO NIVEAU values(00,"SUPER Admin");
+INSERT INTO LIGUE values(01,"Admin");
+INSERT INTO LIGUE values(02,"Simple Employe");
+
+INSERT INTO DateDepart values('2012-02-29',3);
+INSERT INTO DateDepart values('2014-05-02',5);
+INSERT INTO DateDepart values('2017-12-17',9);
+
+INSERT INTO EMPLOYE values(1, "Armand", "BOOBA","B2OBA_BOULBI-ZERR@hotmail.com","92i",'2000-01-01');
+INSERT INTO EMPLOYE values(5, "kylian", "M'BAPPE","KIKI.M'BAP-PSG@hotmail.com","lanoisette",'2010-06-12');
+INSERT INTO EMPLOYE values(9, "Laoni", "LAFOUINE","FouinnyBabe@hotmail.com","barbichette",'2005-11-21');
