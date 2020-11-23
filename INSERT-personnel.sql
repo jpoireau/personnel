ce1f4a7589baf8c125fb6abf1e1786ebba2046f7
RAMSES-666 <72388998+RAMSES-666@users.noreply.github.com>
2020-11-23 15:43:18 +0100

    Update INSERT-personnel.sql

--- INSERT-personnel.sql
+++ INSERT-personnel.sql
@@ -3,5 +3,5 @@ INSERT INTO LIGUE values(02,"ligue de Taekwondo");
 INSERT INTO LIGUE values(03,"ligue de mangeur de hot dog");
 
 INSERT INTO EMPLOYE values(1, "Armand", "BOOBA","B2OBA_BOULBI-ZERR@hotmail.com","92i", 0, 0,'2000-01-01', '2012-02-29');
-INSERT INTO EMPLOYE values(5, "kylian", "M'BAPPE","KIKI.M'BAP-PSG@hotmail.com","lanoisette", 0,0,'2010-06-12', '2014-05-02');
-INSERT INTO EMPLOYE values(9, "Laoni", "LAFOUINE","FouinnyBabe@hotmail.com","barbichette", 0 ,0,'2005-11-21', '2017-12-17');
+INSERT INTO EMPLOYE values(2, "kylian", "M'BAPPE","KIKI.M'BAP-PSG@hotmail.com","lanoisette", 0,0,'2010-06-12', '2014-05-02');
+INSERT INTO EMPLOYE values(3, "Laoni", "LAFOUINE","FouinnyBabe@hotmail.com","barbichette", 0 ,0,'2005-11-21', '2017-12-17');
