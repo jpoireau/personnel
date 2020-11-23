d7a8665ca428dd77ce8f00ec51e5b36895d632e6
RAMSES-666 <72388998+RAMSES-666@users.noreply.github.com>
2020-11-23 15:41:51 +0100

    Add files via upload
    CREATE TABLE FINAL I HOPE

---
+++ Create-Table-personnel.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS LIGUE;
+DROP TABLE IF EXISTS NIVEAU;
+DROP TABLE IF EXISTS DateDepart;
+DROP TABLE IF EXISTS EMPLOYE;
+
+CREATE TABLE LIGUE(
+   IdLigue INT,
+   NomLigue VARCHAR(50),
+   constraint pk_Ligue PRIMARY KEY(IdLigue)
+)ENGINE=INNODB;
+
+
+
+
+CREATE TABLE EMPLOYE(
+   NumEmp INT,
+   PrenomEmp VARCHAR(25),
+   NomEmp VARCHAR(25),
+   MailEmp VARCHAR(50),
+   PasswordEmp VARCHAR(25),
+   Admin TINYINT(1),
+   Super_Admin TINYINT(1), 
+   DateArrive DATE ,
+   DateDepart DATE,
+   constraint pk_EMPLOYE PRIMARY KEY(NumEmp)
+   )ENGINE=INNODB;
