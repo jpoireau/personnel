2518ec9ef5e154ef9bf2d10f946367b589153778
jpoireau <72388830+jpoireau@users.noreply.github.com>
2021-04-23 19:11:39 +0200

    Add files via upload

---
+++ ScriptBDD + MCD/InsertPersonnel.sql
@@ -0,0 +1,7 @@
+INSERT INTO LIGUE values(01,"ligue de Vo Thuat");
+INSERT INTO LIGUE values(02,"ligue de Taekwondo");
+INSERT INTO LIGUE values(03,"ligue de mangeur de hot dog");
+
+INSERT INTO EMPLOYE values(1, "Armand", "BOOBA","92i","B2OBA_BOULBI-ZERR@hotmail.com", '2000-01-01', '2012-02-29',0, 0, 1);
+INSERT INTO EMPLOYE values(2, "kylian", "MBAPPE","lanoisette","KIKI.MBAP-PSG@hotmail.com", '2010-06-12', '2014-05-02',1, 0, 2);
+# INSERT INTO EMPLOYE values(9, "Laoni", "LAFOUINE","FouinnyBabe@hotmail.com","barbichette",1, 0 ,0, 3, '2005-11-21', '2017-12-17');
