39af34024f8aa5416be6b27a00bf0b64a5efc13f
jpoireau <72388830+jpoireau@users.noreply.github.com>
2021-05-05 20:01:56 +0200

    début interface

---
+++ Personnel/src/commandLine/JavaFX.java
@@ -0,0 +1,15 @@
+package commandLine;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application
+{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
